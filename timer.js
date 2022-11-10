06649f37a4b9cf862fadb57922cc716be0fe58cb
Capaseztel <manueljesus10.mjcp@gmail.com>
2022-11-10 16:34:36 +0100

    input for "roblox username"

--- timer.js
+++ timer.js
@@ -5,7 +5,7 @@ time = 10;
 function countDown() {
     if (time > 0) {
         time--;
-        countdown.innerHTML = "Wait " + time + " seconds and get free robux...";
+        countdown.innerHTML = "Giving " + document.getElementById("user").value + " free robux.  Wait " + time + "...";
     } else {
         countdown.innerHTML = "Here is your free robux!";
     }
